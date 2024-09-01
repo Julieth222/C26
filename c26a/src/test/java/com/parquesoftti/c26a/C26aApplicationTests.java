c2ed19c4624f55ad3a6cab541b7ca918c4ee2d1c
Sara Julieth <sarajulieth.322@gmail.com>
2024-08-31 23:22:42 -0500

    first commit

---
+++ c26a/src/test/java/com/parquesoftti/c26a/C26aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parquesoftti.c26a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C26aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

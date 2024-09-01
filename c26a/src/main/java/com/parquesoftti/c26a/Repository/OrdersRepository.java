007ba0d5ab1030787aa03b18625f41fd116d5115
Sara Julieth <sarajulieth.322@gmail.com>
2024-08-31 23:53:52 -0500

    Model

--- c26a/src/main/java/com/parquesoftti/c26a/Repository/OrdersRepository.java
+++ c26a/src/main/java/com/parquesoftti/c26a/Repository/OrdersRepository.java
@@ -1,4 +1,12 @@
 package com.parquesoftti.c26a.Repository;
 
-public interface OrdersRepository {
+import com.parquesoftti.c26a.model.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    Optional<Orders> findByQuantity(String quantity);
 }
--- c26a/src/main/java/com/parquesoftti/c26a/Repository/ProductRepository.java
+++ c26a/src/main/java/com/parquesoftti/c26a/Repository/ProductRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository  extends JpaRepository<Product,Long> {
-    @Override
-    Optional <Product> findById(Long Id);
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByOrders(String productName);
 }

28450bd2efc7d701eb72fc874632a1d64d5d4dba
Sara Julieth <sarajulieth.322@gmail.com>
2024-09-01 00:08:37 -0500

    Repository

--- c26a/src/main/java/com/parquesoftti/c26a/Repository/ProductRepository.java
+++ c26a/src/main/java/com/parquesoftti/c26a/Repository/ProductRepository.java
@@ -4,8 +4,5 @@ import com.parquesoftti.c26a.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository  extends JpaRepository <Product, Long>{
 }

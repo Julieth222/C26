e6f2f2ebb9e548b2b29cd8b283bd1db2ed243fab
Sara Julieth <sarajulieth.322@gmail.com>
2024-08-31 23:40:46 -0500

    Model

---
+++ c26a/src/main/java/com/parquesoftti/c26a/Repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.parquesoftti.c26a.Repository;
+
+import com.parquesoftti.c26a.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository  extends JpaRepository<Product,Long> {
+    @Override
+    Optional <Product> findById(Long Id);
+}
